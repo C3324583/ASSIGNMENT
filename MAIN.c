431b144d75d26845351d4b572da480f5863d298b
C3324583 <48045839+C3324583@users.noreply.github.com>
2019-04-05 19:51:29 +1100

    FULLY FUNCTIONING ROTATION CIPHER ENCRYPTER
    Fixed bug that caused elements to excede the maximum char ascii number.

--- MAIN.c
+++ MAIN.c
@@ -5,8 +5,8 @@
 
 int main(){
     //initialising variables
-	char text[126], element=0;
-	int n=0, i=0, rotationkey=0;
+	char text[126], element;
+	int n, i, rotationkey;
 	
 	printf("Enter message: ");//prompt the user to imput a message
 	fgets(text,126,stdin);//store the text imputted by the user in the array 'text'
@@ -19,8 +19,9 @@ int main(){
 	    rotationkey = rotationkey - (26 * n);
 	}
 	else if(rotationkey<0){
-	    n = 1 + (-rotationkey) / 26;
-	    rotationkey = rotationkey + (26 * n);
+	    n = -rotationkey / 26;
+	    rotationkey = -rotationkey - (26 * n);
+	    rotationkey = 26 - rotationkey;
 	}
 	
 	//this for loop executes the rotation for all letters in the array 'text' until the element occupying index i is empty
@@ -30,8 +31,8 @@ int main(){
 		//Each lowercase letter of the string is rotated by the same amount
 		if(element >= 'a' && element <= 'z'){
 			element = element + rotationkey;
-			//If the rotation causes the element to have a value > z (according to the ASCII standard), 26 is subtracted to give the rotated letter
-			if(element > 'z'){
+			//If the rotation causes the element to have a value outside 'a'....'z' (according to the ASCII standard), 26 is subtracted to give the rotated letter
+			if(element > 'z' || element < 'a'){
 				element = element - 26;
 			}
 			
@@ -41,8 +42,8 @@ int main(){
 		//Each UPPERCASE letter is turned into lowercase by by adding 32 (according to the ASCII standard) and then rotated
 		else if(element >= 'A' && element <= 'Z'){
 			element = element + rotationkey + 32;
-			//If the rotation causes the element to have a value > z (according to the ASCII standard), 26 is subtracted to give the rotated letter
-			if(element > 'z'){
+			//If the rotation causes the element to have a value outside 'a'....'z' (according to the ASCII standard), 26 is subtracted to give the rotated letter
+			if(element > 'z' || element < 'a'){
 				element = element - 26;
 			}
 			
