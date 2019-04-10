3328b5c05b46a916439434bff6f9103f6ac1069c
C3324583 <48045839+C3324583@users.noreply.github.com>
2019-04-10 18:49:40 +1000

    Create JOINTASSIGNMENT.c

---
+++ JOINTASSIGNMENT.c
@@ -0,0 +1,7 @@
+printf("Select one of the following options:\n")
+printf("Type 1 to encrypt a message with a rotation cipher\n")
+printf("Type 2 to decrypt a message with a rotation cipher\n")
+printf("Type 3 to encrypt a message with a substitution cipher\n")
+printf("Type 4 to decrypt a message with a substitution cipher\n")
+scanf("%d", n)
+switch case
