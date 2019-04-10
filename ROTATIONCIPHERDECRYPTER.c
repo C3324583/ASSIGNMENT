baf2fd2c66b06f181d82887f6b33fff99d4a3fb4
William Millard Jorda <c3324583@uon.edu.au>
2019-04-10 08:56:56 +0000

    got started

--- ROTATIONCIPHERDECRYPTER.c
+++ ROTATIONCIPHERDECRYPTER.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+    //initialising variables
+	char text[126], element;
+	int n, i, rotationkey;
+	
+	printf("Enter message to be decrypted: ");//prompt the user to imput a message
+	scanf(" %[^\n]s", text);//store the text imputted by the user in the array 'text'
+	
+	
+	
+	printf("Decrypted message: %s", text); //prints the rotated message to the screen
+	
+	return 0;
+}
 
