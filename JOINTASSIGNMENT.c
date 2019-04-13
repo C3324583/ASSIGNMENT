93a32f52cb827ffde22b0558d62d32642fe5a181
C3324583 <48045839+C3324583@users.noreply.github.com>
2019-04-13 14:58:54 +1000

    Update JOINTASSIGNMENT.c

--- JOINTASSIGNMENT.c
+++ JOINTASSIGNMENT.c
@@ -8,10 +8,10 @@ int main(){
     printf("Select one of the following options and press enter:\n");
     printf("Type 1 to encrypt a message with a rotation cipher.\n");
     printf("Type 2 to decrypt a message encrypted with a rotation cipher, if the rotation key is known.\n");
-    printf("Type 2 to decrypt a message encrypted with a rotation cipher, if the rotation key is NOT known.\n");
-    printf("Type 3 to encrypt a message with a substitution cipher.\n");
-    printf("Type 4 to decrypt a message encrypted with a substitution cipher, if the alphabet substitution is known.\n");
-    printf("Type 4 to decrypt a message encrypted with a substitution cipher, if the alphabet substitution is NOT known.\n");
+    printf("Type 3 to decrypt a message encrypted with a rotation cipher, if the rotation key is NOT known.\n");
+    printf("Type 4 to encrypt a message with a substitution cipher.\n");
+    printf("Type 5 to decrypt a message encrypted with a substitution cipher, if the alphabet substitution is known.\n");
+    printf("Type 6 to decrypt a message encrypted with a substitution cipher, if the alphabet substitution is NOT known.\n");
     scanf("%d", &n);
     switch(n){
         case 1: RotationEncrypter(); break;
@@ -31,14 +31,15 @@ int main(){
 ____________________________________________________________________________________________________________________*/
 
 void RotationEncrypter(void){
+    
     //initialising variables
 	char text[1000], element;
 	int n, i, rotationkey;
 	
 	printf("Enter message: ");//prompt the user to imput a message
-	scanf(" %[^\n]s", text);//store the text imputted by the user in the array 'text'
+	scanf(" %[^\n]s", text);//store the text imputted by the user in the array "text"
 	printf("Enter rotation key: ");//prompt the user to imput a rotation key
-	scanf("%d", &rotationkey);//store the rotation value in the variable 'rotation'
+	scanf("%d", &rotationkey);//store the rotation value in the variable "rotation"
 	
 	//accounting for rotation keys that are not between 0 and 25 and translating them into their corresponding key within the range
 	if(rotationkey>25){
@@ -51,25 +52,25 @@ void RotationEncrypter(void){
 	    rotationkey = 26 - rotationkey;
 	}
 	
-	//this for loop executes the rotation for all letters in the array 'text' until the element occupying index i is empty
+	//this for loop executes the rotation for all letters in the array "text" until the element occupying index i is empty
 	for(i = 0; text[i] != '\0'; ++i){
 		element = text[i]; //assigning the value at index "i" of "text" to the variable element
 		
-		//Each lowercase letter of the string is rotated by the "rotationkey"
+		//Each lowercase letter of the string is turned into UPPERCASE by subtracting 32 (according to the ASCII standard) and then rotated by the "rotationkey"
 		if(element >= 'a' && element <= 'z'){
-			element = element + rotationkey;
+			element = element + rotationkey - 32;
 			//If the rotation causes the element to have a value outside 'a'....'z' (according to the ASCII standard), 26 is subtracted to give the rotated letter
-			if(element > 'z' || element < 'a'){
+			if(element > 'Z' || element < 'A'){
 				element = element - 26;
 			}
 			text[i] = element; //storing the rotated letter back in the array "text"
 		}
 		
-		//Each UPPERCASE letter is turned into lowercase by by adding 32 (according to the ASCII standard) and then rotated by the "rotationkey"
+		//Each UPPERCASE letter is rotated by the "rotationkey"
 		else if(element >= 'A' && element <= 'Z'){
-			element = element + rotationkey + 32;
+			element = element + rotationkey;
 			//If the rotation causes the element to have a value outside 'a'....'z' (according to the ASCII standard), 26 is subtracted to give the rotated letter
-			if(element > 'z' || element < 'a'){
+			if(element > 'Z' || element < 'A'){
 				element = element - 26;
 			}
 			text[i] = element; //storing the rotated letter back in the array "text"
