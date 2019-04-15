a88f3f0b6bc49e7bbfba02611b97d6c0751107a6
William Millard Jorda <c3324583@uon.edu.au>
2019-04-15 07:10:43 +0000

    second case working

--- JOINTASSIGNMENT.c
+++ JOINTASSIGNMENT.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//function prototypes for the 6 tasks
 void RotationEncrypter(void);
 void RotationDecrypterWithKey(void);
+void RotationDecrypterNoKey(void);
+void SubstitutionEncrypter(void);
+void SubstitutionDecrypterWithAlphabet(void);
+void SubstitutionDecrypterNoAlphabet(void);
+
 int main(){
     int n;
     printf("Select one of the following options and press enter:\n");
@@ -16,10 +22,10 @@ int main(){
     switch(n){
         case 1: RotationEncrypter(); break;
         case 2: RotationDecrypterWithKey(); break;
-        case 3: break;
-        case 4: break;
-        case 5: break;
-        case 6: break;
+        case 3: RotationDecrypterNoKey(); break;
+        case 4: SubstitutionEncrypter(); break;
+        case 5: SubstitutionDecrypterWithAlphabet(); break;
+        case 6: SubstitutionDecrypterNoAlphabet(); break;
         default: printf("Please run again and select one of the listed options");
     }
     return 0;
@@ -27,7 +33,7 @@ int main(){
 
 
 /*____________________________________________________________________________________________________________________
-    FUNCTION FOR ROTATION CIPHER ENCRYPTER
+    FUNCTION FOR ROTATION CIPHER ENCRYPTION
 ____________________________________________________________________________________________________________________*/
 
 void RotationEncrypter(void){
@@ -39,7 +45,7 @@ void RotationEncrypter(void){
 	printf("Enter message: ");//prompt the user to imput a message
 	scanf(" %[^\n]s", text);//store the text imputted by the user in the array "text"
 	printf("Enter rotation key: ");//prompt the user to imput a rotation key
-	scanf("%d", &rotationkey);//store the rotation value in the variable "rotation"
+	scanf("%d", &rotationkey);//store the rotation value in the variable "rotationkey"
 	
 	//accounting for rotation keys that are not between 0 and 25 and translating them into their corresponding key within the range
 	if(rotationkey>25){
@@ -54,7 +60,7 @@ void RotationEncrypter(void){
 	
 	//this for loop executes the rotation for all letters in the array "text" until the element occupying index i is empty
 	for(i = 0; text[i] != '\0'; ++i){
-		element = text[i]; //assigning the value at index "i" of "text" to the variable element
+		element = text[i]; //assigning the value of "text" at index "i" to the variable element
 		
 		//Each lowercase letter of the string is turned into UPPERCASE by subtracting 32 (according to the ASCII standard) and then rotated by the "rotationkey"
 		if(element >= 'a' && element <= 'z'){
@@ -85,25 +91,95 @@ void RotationEncrypter(void){
 
 
 /*____________________________________________________________________________________________________________________
-    FUNCTION FOR ROTATION CIPHER DECRYPTER
+    FUNCTION FOR ROTATION CIPHER DECRYPTION WITH GIVEN ROTATION KEY
 ____________________________________________________________________________________________________________________*/
 
 
 void RotationDecrypterWithKey(void){
-    
+        //initialising variables
+	char text[1000], element;
+	int n, i, rotationkey;
+	
+	printf("Enter message to decrypt: ");//prompt the user to imput a message
+	scanf(" %[^\n]s", text);//store the text imputted by the user in the array "text"
+	printf("Enter rotation key: ");//prompt the user to imput a rotation key
+	scanf("%d", &rotationkey);//store the rotation value in the variable "rotationkey"
+	
+	//accounting for rotation keys that are not between 0 and 25 and translating them into their corresponding key within the range
+	if(rotationkey>25){
+	    n = rotationkey / 26;
+	    rotationkey = rotationkey - (26 * n);
+	}
+	else if(rotationkey<0){
+	    n = -rotationkey / 26;
+	    rotationkey = -rotationkey - (26 * n);
+	    rotationkey = 26 - rotationkey;
+	}
+	
+	//this for loop executes the rotation for all letters in the array "text" until the element occupying index i is empty
+	for(i = 0; text[i] != '\0'; ++i){
+		element = text[i]; //assigning the value of "text" at index "i" to the variable element
+		
+		//Each lowercase letter of the string is turned into UPPERCASE by subtracting 32 (according to the ASCII standard) and then rotated backwards by the "rotationkey"
+		if(element >= 'a' && element <= 'z'){
+			element = element - rotationkey - 32;
+			//If the rotation causes the element to have a value outside 'a'....'z' (according to the ASCII standard), 26 is added to give the rotated letter
+			if(element > 'Z' || element < 'A'){
+				element = element + 26;
+			}
+			text[i] = element; //storing the rotated letter back in the array "text"
+		}
+		
+		//Each UPPERCASE letter is rotated backwards by the "rotationkey"
+		else if(element >= 'A' && element <= 'Z'){
+			element = element - rotationkey;
+			//If the rotation causes the element to have a value outside 'a'....'z' (according to the ASCII standard), 26 is added to give the rotated letter
+			if(element > 'Z' || element < 'A'){
+				element = element + 26;
+			}
+			text[i] = element; //storing the rotated letter back in the array "text"
+		}
+	}
+	
+	printf("The decrypted message is: %s", text); //prints the rotated message to the screen 
 }
 
 
 
 
 /*____________________________________________________________________________________________________________________
-    FUNCTION FOR SUBSTITUTION CIPHER ENCRYPTER
+    FUNCTION FOR ROTATION CIPHER DECRYPTION WITHOUT A GIVEN ROTATION KEY
 ____________________________________________________________________________________________________________________*/
 
 
-
+void RotationDecrypterNoKey(void){
+}
 
 
 /*____________________________________________________________________________________________________________________
-    FUNCTION FOR SUBSTITUTION CIPHER ENCRYPTER
+    FUNCTION FOR SUBSTITUTION CIPHER ENCRYPTION
 ____________________________________________________________________________________________________________________*/
+
+
+void SubstitutionEncrypter(void){
+}
+
+/*____________________________________________________________________________________________________________________
+    FUNCTION FOR SUBSTITUTION CIPHER DECRYPTION WITH GIVEN ALPHABET SUBSTITUTION
+____________________________________________________________________________________________________________________*/
+
+
+
+void SubstitutionDecrypterWithAlphabet(void){
+    
+}
+
+
+/*____________________________________________________________________________________________________________________
+    FUNCTION FOR SUBSTITUTION CIPHER DECRYPTION WITHOUT A GIVEN ALPHABET SUBSTITUTION
+____________________________________________________________________________________________________________________*/
+
+
+void SubstitutionDecrypterNoAlphabet(void){
+    
+}
