af5eef6e85cd28b445f9762b95435e4f067588a9
William Millard Jorda <c3324583@uon.edu.au>
2019-04-28 12:03:16 +0000

    nearly done

--- JOINTASSIGNMENT.c
+++ JOINTASSIGNMENT.c
@@ -16,7 +16,7 @@ void SubstitutionDecrypterNoAlphabet(void);
 
 int main(){
     int n;
-    printf("Select one of the following options and press enter:\n");
+    printf("\nSelect one of the following options and then press <enter>:\n");
     printf("Type 1 to encrypt a message with a rotation cipher.\n");
     printf("Type 2 to decrypt a message encrypted with a rotation cipher, if the rotation key is known.\n");
     printf("Type 3 to decrypt a message encrypted with a rotation cipher, if the rotation key is NOT known.\n");
@@ -33,8 +33,12 @@ int main(){
         case 6: SubstitutionDecrypterNoAlphabet(); break;
         default: printf("Please run again and select one of the listed options"); //if the user inputs anything that is not listed, the text is printed to the screen
     }
+    printf("\n\n");
     return 0;
 }
+
+
+
 
 
 /*____________________________________________________________________________________________________________________
@@ -47,9 +51,9 @@ void RotationEncrypter(void){
 	char text[1000], element;
 	int n, i, rotationkey;
 	
-	printf("Enter message: ");//prompts the user to imput a message
+	printf("\nEnter message: ");//prompts the user to imput a message
 	scanf(" %[^\n]s", text);//stores the text imputted by the user in the array "text"
-	printf("Enter rotation key: ");//prompts the user to imput a rotation key
+	printf("\nEnter rotation key: ");//prompts the user to imput a rotation key
 	scanf("%d", &rotationkey);//stores the rotation value in the variable "rotationkey"
 	
 	//This pair of "if" statements corrects rotation keys that are not between 0 and 25 and translates them into their corresponding key within the range
@@ -88,7 +92,7 @@ void RotationEncrypter(void){
 		}
 	}
 	
-	printf("Encrypted message: %s", text); //prints the rotated message to the screen
+	printf("\nEncrypted message: %s", text); //prints the rotated message to the screen
 }
 
 
@@ -105,9 +109,9 @@ void RotationDecrypterWithKey(void){
 	char text[1000], element;
 	int n, i, rotationkey;
 	
-	printf("Enter message to decrypt: ");//prompts the user to imput a message
+	printf("\nEnter message to decrypt: ");//prompts the user to imput a message
 	scanf(" %[^\n]s", text);//stores the text imputted by the user in the array "text"
-	printf("Enter rotation key: ");//prompts the user to imput a rotation key
+	printf("\nEnter rotation key: ");//prompts the user to imput a rotation key
 	scanf("%d", &rotationkey);//stores the rotation value in the variable "rotationkey"
 	
 	//This pair or "if" statements corrects rotation keys that are not between 0 and 25 and translates them into their corresponding key within the range
@@ -146,8 +150,10 @@ void RotationDecrypterWithKey(void){
 		}
 	}
 	
-	printf("The decrypted message is: %s", text); //prints the rotated message to the screen 
+	printf("\nDecrypted message: %s", text); //prints the rotated message to the screen 
 }
+
+
 
 
 
@@ -157,8 +163,129 @@ void RotationDecrypterWithKey(void){
 ____________________________________________________________________________________________________________________*/
 
 
+void rotation(int x, char text[]);
+void rotationdefault(char text[]);
+
+
 void RotationDecrypterNoKey(void){
+ 
+    //initialisation of variables and arrays
+	char text[10000];
+	int number_of_letter[127] = {0};
+	int n, i, x, ascii, first=0, second=0, third=0, fourth=0, fifth=0;
+	
+	printf("\nEnter message: ");//prompts the user to imput a message
+	scanf(" %[^\n]s", text);//stores the text imputted by the user in the array "text"
+   
+   /*This for loop executes for all characters in the array "text" until the chracter occupying index i is a
+    "NULL" character*/
+    for(n=0; text[n]!='\0'; n++){
+  
+	    //turns lowercase letters in the array "text" into UPPERCASE by subtracting 32
+     	if(text[n] >= 'a' && text[n] <= 'z'){
+		    text[n] = text[n] - 32;
+     	}
+    }
+    
+	for(ascii='A'; ascii<='Z'; ascii++){
+	    for(i=0; text[i] != '\0'; i++){
+	        if(ascii==text[i]){
+	            number_of_letter[ascii]++;
+	        }
+        } 
+	}
+	
+	for (x=0; x<126; ++x){
+	    if(number_of_letter[x] > number_of_letter[first]){
+	        first = x;
+	    }
+	}
+	
+	for (x=0; x<126; ++x){
+	    if(x != first && number_of_letter[x] <= number_of_letter[first] && number_of_letter[x] > number_of_letter[second]){
+	        second = x;
+	    }
+	}
+	
+    for (x=0; x<126; ++x){
+	    if(x != first && x != second && number_of_letter[x] <= number_of_letter[second] && number_of_letter[x] > number_of_letter[third]){
+	        third = x;
+	    }
+	}	
+	
+	for (x=0; x<126; ++x){
+	    if(x != first && x != second && x != third && number_of_letter[x] <= number_of_letter[third] && number_of_letter[x] > number_of_letter[fourth]){
+	        fourth = x;
+	    }
+	}	
+	
+	for (x=0; x<126; ++x){
+	    if(x != first && x != second && x != third && x != fourth && number_of_letter[x] <= number_of_letter[fourth] && number_of_letter[x] > number_of_letter[fifth]){
+	        fifth = x;
+	    }
+	}
+
+
+    rotation(first, text);
+    rotation(second, text);
+    rotation(third, text);
+    rotation(fourth, text);
+    rotation(fifth, text);
+    printf("\nMy code is shit but one of these should be correct:\n");rotationdefault(text);
 }
+
+
+void rotation(int x, char text[]){
+    char txt[10000];
+    for(int y=0;text[y] != '\0'; y++){
+        txt[y]=text[y];
+    }
+    int rotationkey = x - 'E';
+	int success;
+    for(int i=0; txt[i] != '\0'; i++){
+        if(txt[i] >= 'A' && txt[i] <= 'Z'){
+    		txt[i] = txt[i] - rotationkey;
+    		if(txt[i]>'Z'){
+		        txt[i] = txt[i] - 26;
+		    }
+		    if(txt[i]<'A'){
+		        txt[i] = txt[i] + 26;
+		    }    
+		}
+    }
+
+	printf("\nIs this the correct message?\n%s\n", txt); //prints the rotated message to the screen
+	printf("\nYes - Press 1 and then <enter> \nNo - Press 0 and then <enter> \n");
+	scanf("%d", &success);
+	if(success == 1){
+	    exit(0);
+    }
+}
+
+
+
+//this function prints all possible rotation keys to the screen if the first function fails to find the correct rotation key
+void rotationdefault(char text[]){
+    char txt[10000];
+    for(int y=0;text[y] != '\0'; y++){
+        txt[y]=text[y];
+    }
+
+    for(int x=0; x<=25; x++){
+        for(int i=0; txt[i] != '\0'; i++){
+            if(txt[i] >= 'A' && txt[i] <= 'Z'){
+        		txt[i] = txt[i] + 1;
+    	    	if(txt[i]>'Z'){
+    		        txt[i] = txt[i] - 26;
+    		    }
+    		}
+	    }
+	    printf("\n%s\n", txt);
+    }
+} 
+
+
+
 
 
 
@@ -174,9 +301,10 @@ void SubstitutionEncrypter(void){
 	const char ALPHABET[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int n, i;
 	
-	printf("Enter message: ");//prompts the user to imput a message
+	printf("\nEnter message: ");//prompts the user to imput a message
 	scanf(" %[^\n]s", text);//stores the text imputted by the user in the array "text"
-	printf("Enter an alphabet substitution by typing 26 consecutive letters, where the first will replace A, the second will replace B and so on: ");//prompts the user to imput an alphabet substitution
+	printf("\nEnter an alphabet substitution by typing 26 consecutive letters,\n");
+	printf("where the first will replace A, the second will replace B and so on: \n");//prompts the user to imput an alphabet substitution
 	scanf("%s", substitution);//stores the alphabet substitution in the array "substitution"
     
     /*This for loop executes for all characters in the array "text" until the chracter occupying index i is a
@@ -219,6 +347,7 @@ void SubstitutionEncrypter(void){
 
 
 
+
 /*____________________________________________________________________________________________________________________
     FUNCTION FOR SUBSTITUTION CIPHER DECRYPTION WITH GIVEN ALPHABET SUBSTITUTION
 ____________________________________________________________________________________________________________________*/
@@ -230,9 +359,10 @@ void SubstitutionDecrypterWithAlphabet(void){
 	const char ALPHABET[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int n, i;
 	
-	printf("Enter message: ");//prompts the user to imput a message
+	printf("\nEnter message: ");//prompts the user to imput a message
 	scanf(" %[^\n]s", text);//stores the text imputted by the user in the array "text"
-	printf("Enter an alphabet substitution by typing 26 consecutive letters, where the first will replace A, the second will replace B and so on: ");//prompts the user to imput an alphabet substitution
+	printf("\nEnter the alphabet substitution that was used to encrypby he message by typing 26 consecutive letters,\n");
+	printf("where the first replaced A, the second replaced B and so on: \n");//prompts the user to imput an alphabet substitution
 	scanf("%s", substitution);//stores the alphabet substitution in the array "substitution"
     
     /*This for loop executes for all characters in the array "text" until the chracter occupying index i is a
@@ -280,5 +410,46 @@ ________________________________________________________________________________
 
 
 void SubstitutionDecrypterNoAlphabet(void){
+    //initialisation of variables and arrays
+	char text[1000], substitution[100] = "NWLRBMQHCDAZOKYIUXJFEGPTVS";
+	const char ALPHABET[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int n, i;
+	
+	printf("\nEnter message: ");//prompts the user to imput a message
+	scanf(" %[^\n]s", text);//stores the text imputted by the user in the array "text"
+    printf("\nDecrypted message: ");
     
+    /*This for loop executes for all characters in the array "text" until the chracter occupying index i is a
+    "NULL" character*/
+    for(n=0; text[n]!='\0'; n++){
+  
+	    //turns lowercase letters in the array "text" into UPPERCASE by subtracting 32
+     	if(text[n] >= 'a' && text[n] <= 'z'){
+		    text[n] = text[n] - 32;
+     	}
+     
+        //prints the character at index "n" to the screen if it is not a letter
+        if(text[n] < 'A' || text[n] > 'Z'){
+                printf("%c", text[n]);
+        }
+        
+        /* This loop tests if the character at index "n" is equal to any of the letters in the array "substitution" 
+        (index 0 to index 25), and if true, the character is substituted with the corresponding chracter in the 
+        array "ALPHABET" */
+        for(i=0; i<26; i++){
+            
+            /* This "if" statement turns lowercase letters in the array "substitution" into UPPERCASE by subtracting
+            32 before the substitution is done */
+            if(substitution[i] >= 'a' && substitution[i] <= 'z'){
+		        substitution[i] = substitution[i] - 32;
+	        }
+             
+            /* This "if" statement tests if the charater in "text" at index "n" is equal to any character in 
+            "substitution" by running through all possible indices "i". If true, the character at the same index "i" of 
+            "ALPHABET" is printed to the screen */
+            if (text[n] == substitution[i]){
+                printf("%c", ALPHABET[i]);
+            }
+        }
+    }
 }
